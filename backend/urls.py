8f2c870a2bf59bd1a8f6aca4792b926d214fadbb
imbxl <imbxl@hotmail.com>
2022-12-22 17:17:51 +0100

    API modificada para funcionar con Token

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
+from rest_framework.authtoken import views
 
 from api.router import router_stock
 
@@ -11,5 +12,6 @@ admin.site.index_title = "Bienvenido al sistema de control de Stock"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router_stock.urls)),
+    path('api_token/', views.obtain_auth_token),
     re_path(r".*", TemplateView.as_view(template_name='index.html')),
 ]
